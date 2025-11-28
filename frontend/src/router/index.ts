21d58520167c123cdb2c3115e4838d24141f389f
peet5228 <ppeet5228@gmail.com>
2025-11-28 10:04:13 -0800

    updateIndex

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,16 +1,12 @@
 import LoginForm from '@/LoginForm.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
-<<<<<<< HEAD
 import RegisterForm from '@/RegisterForm.vue'
-=======
->>>>>>> 577af4bf2bd578c942745edc7b947eb02decefb2
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-<<<<<<< HEAD
       path: '/regis',
       name: 'RegisterForm',
       component: RegisterForm,
@@ -21,13 +17,11 @@ const router = createRouter({
       component: LoginForm,
     },
     {
-=======
       path: '/login',
       name: 'LoginForm',
       component: LoginForm,
     },
     {
->>>>>>> 577af4bf2bd578c942745edc7b947eb02decefb2
       path: '/',
       redirect: '/login',
     },
