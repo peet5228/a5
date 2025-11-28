623b724eb40935ab7c19125b3814623cd874ac70
peet5228 <ppeet5228@gmail.com>
2025-11-27 19:03:15 -0800

    'update'

---
+++ backend/routes/Staff/topic.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const db = require('../../db')
+const router = express.Router()
+
+// API สำหรับ GET ข้อมูล ========================================================================
+// router.get('/',async (req,res) => {
+//     try{
+//         const [rows] = await db.query(``)
+//         res.json(rows)
+//     }catch(err){
+//         console.error('Error Get',err)
+//         res.status(500).json({message:'Error Get'})
+//     }
+// })
+
+// API สำหรับ GET ข้อมูล ========================================================================
+router.get('/',async (req,res) => {
+    try{
+        const [rows] = await db.query(`select * from tb_topic order by id_topic desc`)
+        res.json(rows)
+    }catch(err){
+        console.error('Error Get',err)
+        res.status(500).json({message:'Error Get'})
+    }
+})
+
+// API สำหรับ Post ข้อมูล ========================================================================
+router.post('/',async (req,res) => {
+    try{
+        const {name_topic} = req.body
+        const [rows] = await db.query(`insert into tb_topic (name_topic) values (?)`,[name_topic])
+        res.json({rows,message:'Insert Success'})
+    }catch(err){
+        console.error('Error Insert',err)
+        res.status(500).json({message:'Error Insert'})
+    }
+})
+
+// API สำหรับ Update ข้อมูล ========================================================================
+router.put('/:id_topic',async (req,res) => {
+    try{
+        const {id_topic} = req.params
+        const {name_topic} = req.body
+        const [rows] = await db.query(`update tb_topic set name_topic=? where id_topic='${id_topic}'`,[name_topic])
+        res.json({rows,message:'Update Success'})
+    }catch(err){
+        console.error('Error Update',err)
+        res.status(500).json({message:'Error Update'})
+    }
+})
+
+// API สำหรับ Delete ข้อมูล
+router.delete('/:id_topic',async (req,res) => {
+    try{
+        const {id_topic} = req.params
+        const [rows] = await db.query(`delete from tb_topic where id_topic='${id_topic}'`)
+        if(affectedRows === 0) return res.status(403).json({message:'ไม่พบข้อมูลจากไอดีนี้'})
+        res.json({rows,message:'Delete Success'})
+    }catch(err){
+        console.error('Error Delete',err)
+        res.status(500).json({message:'Error Delete'})
+    }
+})
+
+module.exports = router
